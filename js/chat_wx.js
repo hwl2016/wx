9df45ba4fc66234c3b5e8e22ba2a9a573d86b5c2
hwl2016 <hwl2016@sina.com>
2018-01-17 21:34:12 +0800

    bbb

--- js/chat_wx.js
+++ js/chat_wx.js
@@ -1,5 +1,7 @@
 $(function() {
 
+	createEmoji();
+
 	// 编辑区focus时高亮显示
 	$(document).off('focus.editor').on('focus.editor', '#editor', function() {
 		$('.chat_opt').addClass('bk_white')
@@ -8,13 +10,41 @@ $(function() {
 		$('.chat_opt').removeClass('bk_white')
 	})
 
+	$('.js_logo_emoji').click(function(e) {
+		e.stopPropagation()
+		$('.emoji').toggleClass('hidden')
+	})
 
+	$(document).click(function() {
+		$('.emoji').addClass('hidden')
+	})
+
+	$(document).on('click', '.js_emoji_gif', function() {
+		var emoji = $(this).attr('src')
+        $("#editor").focus();
+        var source = '<img src="'+emoji+'">';
+        insertDom(source);
+	})
+
+	$('.js_chat_send').click(function() {
+		send()
+	})
+
+	$(document).on('keyup', '#editor', function(e) {
+        if (e.ctrlKey && e.keyCode == 13) {
+            console.log('Ctrl + Enter...')
+            insertDom('<div><br></div>')
+        }else if (e.keyCode == 13) {
+            console.log('Enter...')
+            send()
+        }
+    })
 
 })
 
 // 在光标位置插入节点
-function _insertimg(str){
-    document.getElementById('js_info').focus();
+function insertDom(str){
+    document.getElementById('editor').focus();
     var selection= window.getSelection ? window.getSelection() : document.selection;
     var range= selection.createRange ? selection.createRange() : selection.getRangeAt(0);
     if (!window.getSelection){
@@ -45,4 +75,49 @@ function _insertimg(str){
         selection.removeAllRanges();
         selection.addRange(range)
     }
+}
+
+// 发送信息
+function send() {
+	var editor = $('#editor')
+	var content = editor.html()
+		.replace(/<div><br><\/div>/g, '\n')
+		.replace(/<\/div>/g, '\n')
+		.replace(/<div>/g, '\n')
+		.trim()
+	
+	if(!content) {
+		console.log('请输入内容！');
+	}else {
+		console.log('send: ' + content)
+		var speak = $('<div class="right"><span>NAME</span> <pre>' + content + '</pre></div>')
+		$('.chat_show').append(speak)
+		speak.get(0).scrollIntoView(true)	//使聊天内容出现在可视区
+	}
+	editor.html('');
+
+}
+
+// 生成emoji
+function createEmoji() {
+	// $.ajax({
+	// 	url: './data/emoji.json',
+	// 	type:'GET',
+	// 	success: function(emoji) {
+	// 		var emojiDOM = '';
+	// 		for(var i=0;i<emoji.length;i++) {
+	// 	        var item = emoji[i]
+	// 	        emojiDOM += '<li><a href="javascript:;"><img src="' + item.path + '" alt="" title="'+item.name+'" class="js_emoji_gif"></a></li>'
+	// 	    }
+	// 	    $('.emoji').html(emojiDOM)
+	// 	}
+	// })
+
+	var emojiDOM = '';
+	for(var i=0; i<emoji.length; i++) {
+        var item = emoji[i]
+        emojiDOM += '<li><a href="javascript:;"><img src="' + item.path + '" alt="" title="'+item.name+'" class="js_emoji_gif"></a></li>'
+    }
+    $('.emoji').html(emojiDOM)
+	
 }
